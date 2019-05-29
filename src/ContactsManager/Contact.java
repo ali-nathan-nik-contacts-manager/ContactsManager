fd1dc45775b127bba6a24aeb057741d468fa56a6
rosalesnikho <rosales.nikho@gmail.com>
2019-05-29 10:27:53 -0500

    added add contact functionality

---
+++ src/ContactsManager/Contact.java
@@ -0,0 +1,44 @@
+package ContactsManager;
+
+import java.util.ArrayList;
+
+public class Contact {
+
+    private static ArrayList<Contact> ContactList = new ArrayList<>();
+    private static Integer telNum = ContactList.size();
+
+    private String firstName;
+    private String lastName;
+    private String phoneNumber;
+
+    public Contact(String firstName, String lastName, String phoneNumber) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.phoneNumber = phoneNumber;
+    }
+
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setPhoneNumber(String phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    }
+}
--- src/ContactsManager/Manager.java
+++ src/ContactsManager/Manager.java
@@ -4,20 +4,109 @@ import java.util.*;
 import java.nio.file.*;
 import java.io.IOException;
 
-
 public class Manager {
+    static String directory = "data";
+    static Path dataFolder = Paths.get(directory);
+    static Path dataFile = Paths.get(directory, "contact.txt");
+
+    /////////// M A I N /////////////
+
     public static Scanner scan = new Scanner(System.in);
+
     public static void main(String[] args) {
 
-        String directory = "data";
-
-        // Set directory
-        Path dataFolder = Paths.get(directory);
-        Path dataFile = Paths.get(directory, "contact.txt");
 
 
+
+        // Append New Contact
+//        List<String> updatedList = new ArrayList<>();
+//
+//        try {
+//            List<String> namesFromFile = Files.readAllLines(dataFile);
+//
+//            for (String line : namesFromFile) {
+//
+//                String name = line.split(" ")[0];
+//                int PhoneNumber = Integer.parseInt(line.split(" ")[0]);
+//
+//                System.out.println("Name " + " | " + "Number");
+//                System.out.println(name + " | " + PhoneNumber);
+//
+//                if (name.equalsIgnoreCase("ryan")) {
+//                    updatedList.add(name + " " + 35);
+//                    continue;
+//                }
+//
+//                updatedList.add(line);
+//            }
+//
+//            Files.write(dataFile, updatedList);
+//
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+//
+//        try {
+//            Files.write(dataFile, Arrays.asList(addContact()), StandardOpenOption.APPEND);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//        }
+        userChoice();
+    }
+
+    ////// End of Main ///////
+
+
+    //CLI
+    public static void userChoice() {
+
+        System.out.println("Welcome to Contacts Manager!");
+
+        System.out.println(
+                " 1. View contacts.\n" +
+                        "        2. Add a new contact.\n" +
+                        "        3. Search a contact by name.\n" +
+                        "        4. Delete an existing contact.\n" +
+                        "        5. Exit." +
+                        " Enter an option (1, 2, 3, 4 or 5): "
+        );
+
+        int choice = scan.nextInt();
+
+        switch (choice) {
+
+            case (1): {
+                displayContacts();
+            }
+            case (2): {
+                addContact();
+                break;
+            }
+            case (3): {
+                contactByName("name");
+                break;
+            }
+            case (4): {
+                System.out.println("Enter the name.");
+                String name = scan.nextLine();
+                deleteContact("name");
+            }
+            case (5): {
+                System.out.println("Exiting application...");
+
+            }
+            default: {
+                System.out.println("Please enter a valid number of choice.");
+                userChoice();
+            }
+        }
+    }
+
+    String userChoice = scan.nextLine();
+
+    public static void folderFile(){
         // Creating a folder, check first if folder exist continue
-        if(Files.notExists(dataFolder)){
+        if (Files.notExists(dataFolder)) {
             try {
                 Files.createDirectories(dataFolder);
                 System.out.println(dataFolder + " was created");
@@ -27,125 +116,57 @@ public class Manager {
         }
 
         // Create file, check if it exist first
-        if(!Files.exists(dataFile)){
+        if (!Files.exists(dataFile)) {
             try {
                 Files.createFile(dataFile);
                 System.out.println(dataFile + " file was created");
             } catch (IOException e) {
                 e.printStackTrace();
             }
-        }
-        else{
+        } else {
             System.out.println(dataFile + " file already exists");
         }
+    }
 
+    // Display Contacts
+    public static ArrayList<Contact> displayContacts() {
 
-        //
-        List<String> updatedList = new ArrayList<>();
+        ArrayList<Contact> contacts = new ArrayList<>();
 
         try {
-            List<String> namesFromFile = Files.readAllLines(dataFile);
-            for (String line: namesFromFile) {
-
-                String name = line.split(" ")[0];
-                int age = Integer.parseInt(line.split(" ")[1]);
-
-                System.out.println("name " + " | " + "Number");
-                System.out.println(name + " | " + age);
-
-                if(name.equalsIgnoreCase("ryan")){
-                    updatedList.add(name + " " + 35);
-                    continue;
-                }
-                updatedList.add(line);
+            List<String> FullContactsList = Files.readAllLines(dataFile);
+            for (int i = 0; i < FullContactsList.size(); i++) {
+                System.out.println(FullContactsList);
             }
-            Files.write(dataFile, updatedList);
-
-        } catch (IOException e) {
-            e.printStackTrace();
+        } catch (Exception e) {
+            System.out.println("FullContactsList Exception: " + e);
         }
-
-        try {
-            Files.write(dataFile, Arrays.asList(addContact()), StandardOpenOption.APPEND);
-        } catch (IOException e) {
-            e.printStackTrace();
-        }
+        return contacts;
     }
 
-
-
-
-      public void userChoice () {
-
-          System.out.println(" 1. View contacts.\n" +
-                  "        2. Add a new contact.\n" +
-                  "        3. Search a contact by name.\n" +
-                  "        4. Delete an existing contact.\n" +
-                  "        5. Exit." +
-                  " Enter an option (1, 2, 3, 4 or 5): "
-          );
-          int choice = scan.nextInt();
-
-          switch(choice){
-
-              case (1): {
-                  displayContacts();
-              }
-              case (2): {
-                  addContact();
-                  break;
-              }
-              case (3):  {
-                  System.out.println("Enter the name.");
-                  String name = scan.nextLine();
-                  contactByName("name");
-                  break;
-              }
-              case(4): {
-                  System.out.println("Enter the name.");
-                  String name = scan.nextLine();
-                  deleteContact("name");
-              }
-              case (5): {
-                  System.out.println("Exiting application...");
-
-              }
-              default: {
-                  System.out.println("Please enter a valid number of choice.");
-                  userChoice();
-              }
-          }
-      }
-
-
-        String userChoice = scan.nextLine();
-
-
-
-
-
-    // View Contacts
-    public static void displayContacts(){
-        // displays
-
-    }
 
     //Get Data from User to Append to contact
-    public static String addContact(){
-        String contact;
+    public static void addContact() {
+        Scanner choice = new Scanner(System.in);
         System.out.println("Enter contact name & number: ");
         System.out.println("Ex. First Name Last Name | 555.5555");
-        contact = scan.nextLine();
-        return contact;
+        String contact = choice.nextLine();
+
+        try {
+            Files.write(dataFile, Arrays.asList(contact), StandardOpenOption.APPEND);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        userChoice();
     }
 
     // Search contact by name
-    public static void contactByName(String name){
-        // att by name
+    public static void contactByName(String name) {
+
     }
 
     //delete Contact
-    public static void deleteContact(String name){
+    public static void deleteContact(String name) {
 
     }
 }
